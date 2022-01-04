7bf227b5ce1aa81fc5b8a102ab7717ea0f2d7c1e
Rado Rakotozafy <radomiarintsoarakotozafy@gmail.com>
2022-01-04 17:55:11 +0300

    Form validation

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -9,43 +9,55 @@ function Login({ }: LoginProps) {
     const [login, setlogin] = useState("");
     const [password, setpassword] = useState("");
     const [showLoginMessage, setshowLoginMessage] = useState<boolean>(false);
+    const [message, setmessage] = useState("");
     const navigate = useNavigate();
     const handleSubmit = () => {
         console.log('login');
-        axios.post(`http://127.0.0.1:8001/api/login_check`, {
-            username: login,
-            password
-        })
-        .then(async (response) => {
-            console.log(response.data);
-            if(response.data.token){
-                await localStorage.setItem("token", response.data.token);
-                
-                navigate(`/`, {
-                    state: {
-                        datetime: Date.now()
-                    }
-                });
-                
-            }
-            else {
-                // redirect to login - token expired - show message
-                console.log("error");
+        if(login !== "" && password !== ""){
+            axios.post(`http://127.0.0.1:8001/api/login_check`, {
+                username: login,
+                password
+            })
+            .then(async (response) => {
+                console.log(response.data);
+                if(response.data.token){
+                    await localStorage.setItem("token", response.data.token);
+                    
+                    navigate(`/`, {
+                        state: {
+                            datetime: Date.now()
+                        }
+                    });
+                    
+                }
+                else {
+                    // redirect to login - token expired - show message
+                    console.log("error");
+                    setmessage('Login ou mot de passe incorrect.');
+                    setshowLoginMessage(true);
+                    setTimeout(() => {
+                        setshowLoginMessage(false);
+                    }, 3000);
+                }
+
+            })
+            .catch(error => {
+                console.log('errorc');
+                setmessage('Login ou mot de passe incorrect.');
                 setshowLoginMessage(true);
                 setTimeout(() => {
                     setshowLoginMessage(false);
                 }, 3000);
-            }
-
-        })
-        .catch(error => {
-            console.log('errorc');
+                console.error(error);
+            }); 
+        }
+        else {
+            setmessage("Veuillez remplir tous les champs");
             setshowLoginMessage(true);
             setTimeout(() => {
                 setshowLoginMessage(false);
             }, 3000);
-            console.error(error);
-        }); 
+        }
     }
     return <>
         <div className="wrapper fadeInDown">
@@ -59,7 +71,7 @@ function Login({ }: LoginProps) {
                 {
                     showLoginMessage ? 
                     <div className="alert alert-warning">
-                        Login ou mot de passe incorrect.
+                        {message}
                     </div>
                     : null
                 }
--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -14,55 +14,79 @@ function Register({ }: RegisterProps) {
     const [message, setmessage] = useState("");
     const navigate = useNavigate();
     const handleSubmit = () => {
-        console.log('login');
-        axios.post(`http://127.0.0.1:8001/api/register`, {
-            username: login,
-            password,
-            email,
-        })
-        .then((response) => {
-            console.log(response.data);
-            if(response.data.status === 200){
-                setshowSuccessMessage(true);
-                setTimeout(() => {
-                    setshowSuccessMessage(false);
-                    navigate(`/login`, {
-                        state: {
-                            datetime: Date.now()
-                        }
-                    });
-                }, 3000);
-
-                
+        console.log('regsiter');
+        if(login !== "" && password !== "" && email !== ""){
+            if(validateEmail(email)){
+                axios.post(`http://127.0.0.1:8001/api/register`, {
+                    username: login,
+                    password,
+                    email,
+                })
+                .then((response) => {
+                    console.log(response.data);
+                    if(response.data.status === 200){
+                        setshowSuccessMessage(true);
+                        setTimeout(() => {
+                            setshowSuccessMessage(false);
+                            navigate(`/login`, {
+                                state: {
+                                    datetime: Date.now()
+                                }
+                            });
+                        }, 3000);
+        
+                        
+                    }
+                    else {
+                        // redirect to login - token expired - show message
+                        console.log("error");
+                        setmessage(response.data.message);
+                        setshowLoginMessage(true);
+                        setTimeout(() => {
+                            setshowLoginMessage(false);
+                        }, 3000);
+                    }
+        
+                })
+                .catch(error => {
+                    console.log('errorc');
+                    console.log(error);
+                    setshowLoginMessage(true);
+                    setTimeout(() => {
+                        setshowLoginMessage(false);
+                    }, 3000);
+                    console.error(error);
+                }); 
             }
             else {
-                // redirect to login - token expired - show message
-                console.log("error");
-                setmessage(response.data.message);
+                setmessage("Email invalide");
                 setshowLoginMessage(true);
                 setTimeout(() => {
                     setshowLoginMessage(false);
                 }, 3000);
             }
-
-        })
-        .catch(error => {
-            console.log('errorc');
-            console.log(error);
+        }
+        else {
+            setmessage("Veuillez remplir tous les champs");
             setshowLoginMessage(true);
             setTimeout(() => {
                 setshowLoginMessage(false);
             }, 3000);
-            console.error(error);
-        }); 
+        }
     }
+
+    const validateEmail = (email: string) => {
+        return email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
     return <>
         <div className="wrapper fadeInDown">
             <div id="formContent" className='pt-3'>
                 <form>
-                    <input type="text" id="login" className="fadeIn second form-control" name="login" placeholder="login" onChange={e => setlogin(e.target.value)}/>
-                    <input type="text" id="email" className="fadeIn third form-control" name="email" placeholder="email" onChange={e => setemail(e.target.value)} />
-                    <input type="text" id="password" className="fadeIn third form-control" name="password" placeholder="password" onChange={e => setpassword(e.target.value)} />
+                    <input type="text" id="login" className="fadeIn second form-control" name="login" placeholder="login" onChange={e => setlogin(e.target.value)} required/>
+                    <input type="text" id="email" className="fadeIn third form-control" name="email" placeholder="email" onChange={e => setemail(e.target.value)} required />
+                    <input type="text" id="password" className="fadeIn third form-control" name="password" placeholder="password" onChange={e => setpassword(e.target.value)} required />
                     <input type="button" className="fadeIn fourth" value="Register" onClick={handleSubmit} />
                 </form>
 
